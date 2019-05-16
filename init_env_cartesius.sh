d9cd96e34ef560fb7a4c2054622731d94be96b27
Damian Podareanu <damian@tcn676.bullx>
2019-05-16 16:19:28 +0200

    Added Cartesius installation and runfiles

---
+++ init_env_cartesius.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+module load Python/2.7.14-foss-2017b
+
+wget https://surfdrive.surf.nl/files/index.php/s/hm6EAx3j39H5FFh/download --output-document "tensorflow-1.13.1-cp27-cp27mu-linux_x86_64.whl"
+
+virtualenv cp27_gcc63_omp211
+source cp27_gcc63_omp211/bin/activate
+
+pip install tensorflow-1.13.1-cp27-cp27mu-linux_x86_64.whl --ignore-installed --no-cache
+CC=mpicc CXX=mpicxx pip install horovod --no-cache
+pip install sklearn keras
+
+if [ $(python -c "import horovod.tensorflow as hvd;hvd.init();print(hvd.size())") == "1" ]; then
+  echo "Test passed, Horovod initialised"
+else
+  echo "Integrity check failed"
+fi
