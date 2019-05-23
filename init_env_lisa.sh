da3da44a5cd0ab362a1432d280edc538ecd2278b
Damian Podareanu <damian@test3.irc.sara.nl.irc.sara.nl>
2019-05-23 16:42:35 +0200

    LISA integration

---
+++ init_env_lisa.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --job-name SetENV 
+#SBATCH --nodes 1 
+#SBATCH --partition test 
+#SBATCH --time 01:00:00
+#SBATCH --cores-per-socket=16
+
+module load Python/2.7.14-foss-2017b
+
+wget https://surfdrive.surf.nl/files/index.php/s/hm6EAx3j39H5FFh/download --output-document "tensorflow-1.13.1-cp27-cp27mu-linux_x86_64.whl"
+
+# virtualenv cp27_gcc63_omp211
+# source cp27_gcc63_omp211/bin/activate
+
+pip install tensorflow-1.13.1-cp27-cp27mu-linux_x86_64.whl --user --force-reinstall --no-cache --ignore-installed
+pip install scikit-learn keras --no-cache --user
+CC=mpicc CXX=mpicxx pip install horovod --no-cache --force-reinstall --ignore-installed --user
+
+if [ $(python -c "import horovod.tensorflow as hvd;hvd.init();print(hvd.size())") == "1" ]; then
+  echo "Test passed, Horovod initialised"
+else
+  echo "Initialisation check failed"
+fi
---
+++ keras/job_lisa_64w_32n_16bs.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -l
+#SBATCH -J 3DGAN_64w_32n_bs16_sun
+#SBATCH -o 3DGAN_64w_32n_bs16_sun_output.txt
+#SBATCH -e 3DGAN_64w_32n_bs16_sun_errors.txt
+
+#SBATCH -t 1:00:00
+
+#SBATCH --partition test
+
+#SBATCH --nodes 32
+#SBATCH --ntasks-per-node=2
+
+module load Python/2.7.14-foss-2017b
+
+ulimit -l unlimited
+ulimit -s unlimited
+
+export KERAS_BACKEND="tensorflow"
+
+NUM_NODES=32
+WORKERS_PER_SOCKET=2
+
+INTER_T=1
+
+NUM_SOCKETS=`lscpu | grep "Socket(s)" | cut -d':' -f2 | xargs`
+CORES_PER_SOCKET=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | xargs`
+
+CORES_PER_WORKER=$((CORES_PER_SOCKET / WORKERS_PER_SOCKET))
+INTRA_T=$CORES_PER_WORKER
+OMP_NUM_THREADS=8 #32 physical cores
+
+WORKERS_PER_NODE=$((WORKERS_PER_SOCKET * NUM_SOCKETS))
+TOTAL_WORKERS=$((NUM_NODES * WORKERS_PER_NODE))
+
+
+echo "CORES_PER_WORKER: $CORES_PER_WORKER"
+echo "NUM_INTRA_THREADS: $INTRA_T"
+echo "NUM_INTER_THREADS: $INTER_T"
+echo "OMP_NUM_THREADS: $OMP_NUM_THREADS"
+echo "WORKERS_PER_NODE: $WORKERS_PER_NODE"
+echo "TOTAL_WORKERS: $TOTAL_WORKERS"
+
+export OMP_NUM_THREADS=$OMP_NUM_THREADS
+export KMP_AFFINITY="granularity=fine,compact,1,0"
+
+mpirun -np ${TOTAL_WORKERS} \
+ --map-by socket:pe=${INTRA_T} \
+ --bind-to core \
+ --report-bindings \
+ --oversubscribe \
+ -x LD_LIBRARY_PATH \
+ -x OMP_NUM_THREADS \
+ -x KMP_AFFINITY \
+ numactl -l python EcalEnergyTrain_hvd.py \
+ --model EcalEnergyGan \
+ --nbepochs 25 \
+ --warmup 5 \
+ --batchsize 16 \
+ -lr 0.001 \
+ --latentsize 200 \
+ --optimizer=RMSprop \
+ --datapath='/home/damian/cern_1.3/eos/project/d/dshep/LCD/V1/EleEscan/*.h5' \
+ --weightsdir='/home/damian/cern_1.3/trained_models/CERN/3DGAN_64w_32n_bs16_sun' \
+ --intraop ${INTRA_T} \
+ --interop ${INTER_T}
